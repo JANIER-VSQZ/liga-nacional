52b618fe9ab5694d6d88bfe69c81ec16884307c0
JuanRa <jraguilar012012@gmail.com>
2025-06-27 18:49:18 -0600

    Agregar una segunda liga

--- siteData/pages/home.js
+++ siteData/pages/home.js
@@ -1,5 +1,5 @@
 export default {
-  equipos: [
+  honduras: [
     {
       nombre: "Olimpia",
       partidosJugados: 18,
@@ -90,5 +90,175 @@ export default {
       golesContra: 28,
       posicion: 10
     }
+  ],
+  alemania: [
+    {
+      nombre: "Bayern München",
+      partidosJugados: 34,
+      partidosGanados: 14 + 11, // Home + Away wins
+      partidosEmpatados: 2 + 5,
+      golesFavor: 99,
+      golesContra: 32,
+      posicion: 1
+    },
+    {
+      nombre: "Bayer 04 Leverkusen",
+      partidosJugados: 34,
+      partidosGanados: 10 + 9,
+      partidosEmpatados: 4 + 8,
+      golesFavor: 72,
+      golesContra: 43,
+      posicion: 2
+    },
+    {
+      nombre: "Eintracht Frankfurt",
+      partidosJugados: 34,
+      partidosGanados: 10 + 7,
+      partidosEmpatados: 4 + 5,
+      golesFavor: 68,
+      golesContra: 46,
+      posicion: 3
+    },
+    {
+      nombre: "Borussia Dortmund",
+      partidosJugados: 34,
+      partidosGanados: 11 + 6,
+      partidosEmpatados: 3 + 3,
+      golesFavor: 71,
+      golesContra: 51,
+      posicion: 4
+    },
+    {
+      nombre: "Freiburg",
+      partidosJugados: 34,
+      partidosGanados: 9 + 7,
+      partidosEmpatados: 3 + 4,
+      golesFavor: 49,
+      golesContra: 53,
+      posicion: 5
+    },
+    {
+      nombre: "Mainz 05",
+      partidosJugados: 34,
+      partidosGanados: 6 + 8,
+      partidosEmpatados: 8 + 2,
+      golesFavor: 55,
+      golesContra: 43,
+      posicion: 6
+    },
+    {
+      nombre: "RB Leipzig",
+      partidosJugados: 34,
+      partidosGanados: 8 + 5,
+      partidosEmpatados: 6 + 6,
+      golesFavor: 53,
+      golesContra: 48,
+      posicion: 7
+    },
+    {
+      nombre: "Werder Bremen",
+      partidosJugados: 34,
+      partidosGanados: 5 + 9,
+      partidosEmpatados: 6 + 3,
+      golesFavor: 54,
+      golesContra: 57,
+      posicion: 8
+    },
+    {
+      nombre: "Stuttgart",
+      partidosJugados: 34,
+      partidosGanados: 7 + 7,
+      partidosEmpatados: 2 + 6,
+      golesFavor: 64,
+      golesContra: 53,
+      posicion: 9
+    },
+    {
+      nombre: "Borussia M'gladbach",
+      partidosJugados: 34,
+      partidosGanados: 7 + 6,
+      partidosEmpatados: 3 + 3,
+      golesFavor: 55,
+      golesContra: 57,
+      posicion: 10
+    },
+    {
+      nombre: "VfL Wolfsburg",
+      partidosJugados: 34,
+      partidosGanados: 3 + 8,
+      partidosEmpatados: 7 + 3,
+      golesFavor: 56,
+      golesContra: 54,
+      posicion: 11
+    },
+    {
+      nombre: "Augsburg",
+      partidosJugados: 34,
+      partidosGanados: 6 + 5,
+      partidosEmpatados: 5 + 5,
+      golesFavor: 35,
+      golesContra: 51,
+      posicion: 12
+    },
+    {
+      nombre: "Union Berlin",
+      partidosJugados: 34,
+      partidosGanados: 5 + 5,
+      partidosEmpatados: 7 + 4,
+      golesFavor: 36,
+      golesContra: 50,
+      posicion: 13
+    },
+    {
+      nombre: "St. Pauli",
+      partidosJugados: 34,
+      partidosGanados: 3 + 5,
+      partidosEmpatados: 5 + 3,
+      golesFavor: 28,
+      golesContra: 41,
+      posicion: 14
+    },
+    {
+      nombre: "TSG Hoffenheim",
+      partidosJugados: 34,
+      partidosGanados: 4 + 3,
+      partidosEmpatados: 5 + 6,
+      golesFavor: 46,
+      golesContra: 68,
+      posicion: 15
+    },
+    {
+      nombre: "FC Heidenheim",
+      partidosJugados: 34,
+      partidosGanados: 3 + 5,
+      partidosEmpatados: 2 + 3,
+      golesFavor: 37,
+      golesContra: 64,
+      posicion: 16
+    },
+    {
+      nombre: "Holstein Kiel",
+      partidosJugados: 34,
+      partidosGanados: 4 + 2,
+      partidosEmpatados: 2 + 5,
+      golesFavor: 49,
+      golesContra: 80,
+      posicion: 17
+    },
+    {
+      nombre: "VfL Bochum",
+      partidosJugados: 34,
+      partidosGanados: 3 + 2,
+      partidosEmpatados: 4 + 4,
+      golesFavor: 32,
+      golesContra: 68,
+      posicion: 18
+    }
   ]
 }
+
+
+
+
+
+
