8f02fe40a2eef598adb49af9c7a02241407c785e
Rick282003 <richardgalo2003@gmail.com>
2025-06-27 18:34:25 -0600

    Estructura de documentos

---
+++ siteData/index.js
@@ -0,0 +1,20 @@
+import indexData from './pages/home';
+import galeriaData from './pages/galeria';
+function context(page){
+    console.log("Getting context for: " + page);
+    let context = {
+        title: "Mi Sitio ABC",
+    }
+    switch(page){
+        case "/index.html":
+            context = {...context, ...indexData};
+            break;
+        case "/galeria.html":
+            context = {...context, ...galeriaData};
+            break;
+    }
+    console.log("Context: ", JSON.stringify(context, null, 2));
+    return context;
+}
+
+export default context;
